c230cc7c42c40d859d8b77b845e40d0a12fe3745
Yaroslav <dzisyuk.yari@gmail.com>
2022-02-04 22:29:14 +0200

    main-slider extends slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/sliders/slider-main';
 import VideoPlayer from './modules/videoPlayer';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({btns: '.next', page: '.page'});
     slider.render();
     const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
     videoPlayer.init();
